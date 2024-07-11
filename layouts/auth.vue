9f0738212c90ebca47953cc209ab9373a0f29e64
Mohaimen <mohaimen@Mohaimens-MacBook-Air.local>
2024-07-11 22:07:14 +0600

    added login page

---
+++ layouts/auth.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-svh">
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,45 @@
-<template>
-  <h1>Login Page</h1>
-  <Button>Button</Button>
-</template>
+<script setup lang='ts'>
+definePageMeta({
+  layout: 'auth'
+})
 
-<script setup lang="ts">
-import {Button} from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
 </script>
+
+<template>
+  <div class="flex justify-center items-center h-[100vh]">
+    <Card class="w-[350px]">
+      <CardHeader>
+        <CardTitle>Log in</CardTitle>
+        <CardDescription>Login your system</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid items-center w-full gap-4">
+            <div class="flex flex-col space-y-1.5">
+              <Label for="username">Username</Label>
+              <Input type="email" id="username" placeholder="Enter username" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+              <Label for="password">Password</Label>
+              <Input type="password" id="password" placeholder="Enter password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
