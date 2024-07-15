c930448252e5e9f75621822f856d222be5a2ab29
mohaimenkhalid <mohaimen707@gmail.com>
2024-07-16 01:27:54 +0600

    update navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,8 +28,21 @@ async function _signOut() {
   <div>
     <nav class="bg-gray-300 py-3">
       <div class="flex justify-between">
-        <div>
-          <h2 class="text-3xl font-bold">Demo Site</h2>
+        <div class="flex items-center gap-x-6">
+          <div>
+            <nuxt-link to="/dashboard" class="text-3xl font-bold">Demo Site</nuxt-link>
+          </div>
+          <ul class="list-none">
+            <li class="inline-block ms-3">
+              <nuxt-link to="/product" class="bg-gray-700 text-white p-2 rounded-sm">Products</nuxt-link>
+            </li>
+            <li class="inline-block ms-3">
+              <nuxt-link to="/category">Category</nuxt-link>
+            </li>
+            <li class="inline-block ms-3">
+              <nuxt-link to="/users">Users</nuxt-link>
+            </li>
+          </ul>
         </div>
         <div>
           <div class="mr-3">
