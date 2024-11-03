657a592619a6a5b175bc02173a2b22f2135165f9
khalid-jatri <khalidhossain.jatri@gmail.com>
2024-11-03 18:07:11 +0600

    add & update: tripCard in ticketsearch page

--- components/search/search-left/buses/Buses.vue
+++ components/search/search-left/buses/Buses.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import FilterBox from "~/components/search/search-left/buses/FilterBox.vue";
+import TripCards from "~/components/search/search-left/buses/trips/TripsCards.vue";
 </script>
 
 <template>
-  <div>
+  <div class="space-y-6">
     <FilterBox />
+    <TripCards />
   </div>
 </template>
